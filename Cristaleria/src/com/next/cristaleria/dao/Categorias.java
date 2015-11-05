64b9e0b5728e93e366e10ff43769a5247b59d027
Nexxtor <nestor.aldana1@gmail.com>
2015-11-04 18:32:49 -0600

    Frist DAO Clases
    Signed-off-by: Nexxtor <nestor.aldana1@gmail.com>

---
+++ Cristaleria/src/com/next/cristaleria/dao/Categorias.java
@@ -0,0 +1,111 @@
+package com.next.cristaleria.dao;
+
+import java.awt.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.next.cristaleria.model.Categoria;
+
+public class Categorias {
+
+	public Categoria insert(Categoria cat) {
+		String query = "insert into Categoria(nombreCategoria) values(?)";
+
+		try {
+			PreparedStatement ps = Connection.getCon().prepareStatement(query);
+			ps.setString(1, cat.getNombre());
+
+			ps.execute();
+
+			ResultSet rs = ps.getGeneratedKeys();
+			Categoria catN;
+			catN = cat;
+			if (rs.next()) {
+				catN.setId(rs.getInt(1));
+			}
+
+			rs.close();
+			ps.close();
+			Connection.getCon().close();
+			return catN;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public boolean delete(Categoria cat) {
+		String query = "delete from categoria where idCategoria = ? ";
+
+		try {
+			PreparedStatement ps = Connection.getCon().prepareStatement(query);
+			ps.setLong(1, cat.getId());
+
+			ps.execute();
+			ps.close();
+			Connection.getCon().close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public boolean update(Categoria cat) {
+		String query = "update Categoria set nombreCategoria = ? where idCategoria= ?";
+
+		try {
+			PreparedStatement ps = Connection.getCon().prepareStatement(query);
+			ps.setString(1, cat.getNombre());
+			ps.setLong(2, cat.getId());
+
+			ps.execute();
+			ps.close();
+			Connection.getCon().close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public ArrayList<Categoria> select(String where) {
+		try {
+			String query = "SELECT * FROM Categoria" + where;
+			Statement st;
+
+			st = Connection.getCon().createStatement();
+
+			ResultSet rs = st.executeQuery(query);
+			ArrayList<Categoria> res =  new ArrayList<>();
+			
+			while (rs.next()) {
+				Categoria cat = new Categoria();
+				
+				cat.setId(rs.getLong("idCategoria"));
+				cat.setNombre(rs.getString("nombreCategoria"));
+				
+				res.add(cat);
+			}
+			rs.close();
+			st.close();
+			Connection.getCon().close();
+			return res;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+}
---
+++ Cristaleria/src/com/next/cristaleria/dao/Connection.java
@@ -0,0 +1,117 @@
+package com.next.cristaleria.dao;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection {
+	private static java.sql.Connection con;
+	private static String server, dataBase, usuario, password;
+	private static int puerto;
+
+	/**
+	 * @return the con
+	 */
+	public static java.sql.Connection getCon() {
+		try {
+			if (con == null || con.isClosed()) {
+
+				try {
+
+					Class.forName("com.mysql.jdbc.Driver");
+
+					con = DriverManager.getConnection("jdbc:mysql://" + server + ":"+puerto+"/" + dataBase, usuario, password);
+
+				} catch (ClassNotFoundException e) {
+					System.err.println("No se encuetra la librebira " + "com.mysql.jdbc.Driver");
+					e.printStackTrace();
+				} catch (SQLException e) {
+					System.err.println("No se puede conectar a la base datos revise credenciales");
+					e.printStackTrace();
+				}
+
+			}
+		} catch (SQLException e) {
+			System.err.println("Al preguntar si la coneccion esta cerrada");
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	
+	/**
+	 * @return the puerto
+	 */
+	public static int getPuerto() {
+		return puerto;
+	}
+
+
+	/**
+	 * @param puerto the puerto to set
+	 */
+	public static void setPuerto(int puerto) {
+		Connection.puerto = puerto;
+	}
+
+
+	/**
+	 * @return the server
+	 */
+	public static String getServer() {
+		return server;
+	}
+
+	/**
+	 * @param server
+	 *            the server to set
+	 */
+	public static void setServer(String server) {
+		Connection.server = server;
+	}
+
+	/**
+	 * @return the dataBase
+	 */
+	public static String getDataBase() {
+		return dataBase;
+	}
+
+	/**
+	 * @param dataBase
+	 *            the dataBase to set
+	 */
+	public static void setDataBase(String dataBase) {
+		Connection.dataBase = dataBase;
+	}
+
+	/**
+	 * @return the usuario
+	 */
+	public static String getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario
+	 *            the usuario to set
+	 */
+	public static void setUsuario(String usuario) {
+		Connection.usuario = usuario;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public static String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public static void setPassword(String password) {
+		Connection.password = password;
+	}
+
+}
